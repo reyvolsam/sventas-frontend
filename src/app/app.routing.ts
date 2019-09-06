06e83e1e85b661274ad243c2b8c615f481749d15
Samuel Regino Placido <samuel.regino@gnp.com.mx>
2019-09-06 12:53:06 -0500

    Actualziacion angular

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,36 +4,36 @@ import {AuthGuard} from './helpers/auth.guard'
 const appRoutes: Routes = [
     {
         path: '',
-        loadChildren: './modules/home/home.module#HomeModule',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
         canActivate: [AuthGuard]
     },
     {
         path: 'login',
-        loadChildren: './modules/login/login.module#LoginModule'
+        loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
     },
     {
         path: 'user',
-        loadChildren: './modules/user/user.module#UserModule',
+        loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule),
         canActivate: [AuthGuard]
     },
     {
         path: 'catalogs/campus',
-        loadChildren: './modules/catalogs/campus/campus.module#CampusModule',
+        loadChildren: () => import('./modules/catalogs/campus/campus.module').then(m => m.CampusModule),
         canActivate: [AuthGuard]
     },
     {
         path: 'catalogs/grade',
-        loadChildren: './modules/catalogs/grade/grade.module#GradeModule',
+        loadChildren: () => import('./modules/catalogs/grade/grade.module').then(m => m.GradeModule),
         canActivate: [AuthGuard]
     },
     {
         path: 'catalogs/group',
-        loadChildren: './modules/catalogs/school-group/school-group.module#SchoolGroupModule',
+        loadChildren: () => import('./modules/catalogs/school-group/school-group.module').then(m => m.SchoolGroupModule),
         canActivate: [AuthGuard]
     },
     {
         path: 'student',
-        loadChildren: './modules/student/student.module#StudentModule',
+        loadChildren: () => import('./modules/student/student.module').then(m => m.StudentModule),
         canActivate: [AuthGuard]
     },
     {
